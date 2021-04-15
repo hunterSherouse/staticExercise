b8920e7daff4e88c248a3da831c3b9ff85915e91
hunterSherouse <huntersherouse2@gmail.com>
2021-04-15 11:39:41 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TempConverter.FahrenheitToCelsius(80);
+
+            TempConverter.CelsiusToFahrenheit(20);
+        }
+
+       
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static void FahrenheitToCelsius(double ftc)
+        {
+            var fahrenheit = (ftc - 32) * .5556;
+            Console.WriteLine(fahrenheit);
+        }
+        public static void CelsiusToFahrenheit(double ctf)
+        {
+            var celcius = (ctf * 1.8) + 32;
+            Console.WriteLine(celcius);
+        }
+
+
+    }
+}
